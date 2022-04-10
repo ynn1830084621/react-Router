7129f0560a04653e8f02b739fcd81b0bb820aee0
ynn <1830084621@qq.com>
2022-04-10 21:34:32 +0800

    " 后台动态获取路由"

--- demo02/src/AppRouter.js
+++ demo02/src/AppRouter.js
@@ -5,25 +5,44 @@ import Video from './Pages/Video';
 import WorkPlace from './Pages/WorkPlace';
 import './index.css';
 
-
 function AppRouter() {
+    let routeConfig = [
+        {path:"/", title: "博客首页", exact:true, element:<Index/> },
+        {path:"/video/*", title: "视频教程", exact:false, element:<Video/> },
+        {path:"/workplace/*", title: "职场技能", exact:false, element:<WorkPlace/> }
+    ]
     return (
         <Router>
             <div className='mainDiv'>
                 <div className='leftNav'>
                     <h3>一级导航</h3>
                     <ul>
-                        <li><Link to="/">博客首页</Link></li>
+                        {/* <li><Link to="/">博客首页</Link></li>
                         <li><Link to="/video/*">视频教程</Link></li>
-                        <li><Link to="/workplace/*">职场技能</Link></li>
+                        <li><Link to="/workplace/*">职场技能</Link></li> */}
+                        {
+                            routeConfig.map((item, index) => {
+                                return (<li key={index}><Link to={item.path}>{item.title}</Link></li>)
+                            })
+                        }
                     </ul>
                 </div>             
                 <div className='rightMain'>
-                    <Routes>
-                        <Route path="/" exact element={<Index/>} />
+                    {/* <Routes> */}
+                        {/* <Route path="/" exact element={<Index/>} />
                         <Route path="/video/*" element={<Video/>} />
-                        <Route path="/workplace/*" element={<WorkPlace/>} />
-                    </Routes>
+                        <Route path="/workplace/*" element={<WorkPlace/>} /> */}
+                    {/* </Routes> */}
+                    {
+                            routeConfig.map((item, index) => {
+                                return (
+                                    <Routes key={index}>
+                                        <Route path={item.path} exact={item.exact} element={item.element} />
+                                    </Routes>
+                                )
+                            })
+                            
+                        }
                 </div>            
             </div>
         </Router>
